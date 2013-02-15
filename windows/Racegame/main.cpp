a15331fcee94c6a9d21eeb04f8d3b1d5bcc763ac
Peter Heinen <peter.heinen@hotmail.com>
2013-02-15 10:34:45 +0100

    Fixed shaders and shit

--- windows/Racegame/main.cpp
+++ windows/Racegame/main.cpp
@@ -1,65 +1,110 @@
+// Include standard headers
+#include <stdio.h>
+#include <stdlib.h>
+
+// Include GLEW
 #include <GL/glew.h>
+
+// Include GLFW
 #include <GL/glfw.h>
+
+// Include GLM
 #include <glm/glm.hpp>
+using namespace glm;
 
-#include <iostream>
+#include <../../src/headers/Shader.h>
 
-#include "../../src/headers/Game.h"
-
-int main()
+int main( void )
 {
-    if(!glfwInit())
-        throw std::runtime_error("GLFW init failed.");
-    
-    glfwOpenWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwOpenWindowHint(GLFW_OPENGL_VERSION_MAJOR, 3);
-    glfwOpenWindowHint(GLFW_OPENGL_VERSION_MINOR, 2);
-    glfwOpenWindowHint(GLFW_WINDOW_NO_RESIZE, GL_TRUE);
-    if(!glfwOpenWindow(800, 600, 8, 8, 8, 8, 16, 0, GLFW_WINDOW))
-        throw std::runtime_error("Failed to open glfw window.");
-    
-    //GLFW settings
-    glfwDisable(GLFW_MOUSE_CURSOR);
-    glfwSetMousePos(0, 0);
-    glfwSetMouseWheel(0);
-    
-    glewExperimental = GL_TRUE; //stops glew crashing on OSX
-    if(glewInit() != GLEW_OK)
-        throw std::runtime_error("Failed to init glew.");
-    glGetError(); //ignore any errors coming from glewInit, because they can be safely ignored.
-    
-    std::cout << "OpenGL version: " << glGetString(GL_VERSION) << std::endl;
-    std::cout << "GLSL version: " << glGetString(GL_SHADING_LANGUAGE_VERSION) << std::endl;
-    std::cout << "Vendor: " << glGetString(GL_VENDOR) << std::endl;
-    std::cout << "Renderer: " << glGetString(GL_RENDERER) << std::endl;
-    
-    if(!GLEW_VERSION_3_2)
-        throw std::runtime_error("No opengl 3.2");
-    
-    glEnable(GL_DEPTH_TEST);
-    glDepthFunc(GL_LESS);
-    
-	Game* myGame = new Game();
+	// Initialise GLFW
+	if( !glfwInit() )
+	{
+		fprintf( stderr, "Failed to initialize GLFW\n" );
+		return -1;
+	}
 
-	myGame->InitializeProgram();
-	myGame->InitializeVertexBuffer();
+	glfwOpenWindowHint(GLFW_FSAA_SAMPLES, 4);
+	glfwOpenWindowHint(GLFW_OPENGL_VERSION_MAJOR, 3);
+	glfwOpenWindowHint(GLFW_OPENGL_VERSION_MINOR, 3);
+	glfwOpenWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    //main loop
-    while(glfwGetWindowParam(GLFW_OPENED))
-    {
-        //glfwSwapBuffers();
-        
-        GLenum error = glGetError();
-        if(error != GL_NO_ERROR)
-            std::cerr << "OpenGL Error " << error << ": " << (const char*)gluErrorString(error) << std::endl;
-        
-        if(glfwGetKey(GLFW_KEY_ESC))
-            glfwCloseWindow();
-		
-		myGame->display();
-    }
-    
-    glfwTerminate();
-    
-    return 0;
+	// Open a window and create its OpenGL context
+	if( !glfwOpenWindow( 1024, 768, 0,0,0,0, 32,0, GLFW_WINDOW ) )
+	{
+		fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n" );
+		glfwTerminate();
+		return -1;
+	}
+
+	// Initialize GLEW
+	if (glewInit() != GLEW_OK) {
+		fprintf(stderr, "Failed to initialize GLEW\n");
+		return -1;
+	}
+
+	glfwSetWindowTitle( "Tutorial 01" );
+
+	// Ensure we can capture the escape key being pressed below
+	glfwEnable( GLFW_STICKY_KEYS );
+
+	// Dark blue background
+	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+
+	GLuint VertexArrayID;
+	glGenVertexArrays(1, &VertexArrayID);
+	glBindVertexArray(VertexArrayID);
+
+	// Create and compile our GLSL program from the shaders
+	GLuint programID = LoadShaders( "C:/Users/Peter/Documents/GitHub/Racegame/src/headers/SimpleVertexShader.vertexshader", "C:/Users/Peter/Documents/GitHub/Racegame/src/headers/SimpleFragmentShader.fragmentshader" );
+	
+	static const GLfloat g_vertex_buffer_data[] = { 
+		-1.0f, -1.0f, 0.0f,
+		 1.0f, -1.0f, 0.0f,
+		 0.0f,  1.0f, 0.0f,
+	};
+
+	GLuint vertexbuffer;
+	glGenBuffers(1, &vertexbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+
+	do{
+		// Clear the screen
+		glClear( GL_COLOR_BUFFER_BIT );
+
+		// Use our shader
+		glUseProgram(programID);
+
+		// 1rst attribute buffer : vertices
+		glEnableVertexAttribArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+		glVertexAttribPointer(
+			0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+			3,                  // size
+			GL_FLOAT,           // type
+			GL_FALSE,           // normalized?
+			0,                  // stride
+			(void*)0            // array buffer offset
+		);
+
+		// Draw the triangle !
+		glDrawArrays(GL_TRIANGLES, 0, 3); // 3 indices starting at 0 -> 1 triangle
+
+		glDisableVertexAttribArray(0);
+
+
+		// Swap buffers
+		glfwSwapBuffers();
+	} // Check if the ESC key was pressed or the window was closed
+	while( glfwGetKey( GLFW_KEY_ESC ) != GLFW_PRESS &&
+		   glfwGetWindowParam( GLFW_OPENED ) );
+
+	// Close OpenGL window and terminate GLFW
+	glfwTerminate();
+	
+	// Cleanup VBO
+	glDeleteBuffers(1, &vertexbuffer);
+	glDeleteVertexArrays(1, &VertexArrayID);
+
+	return 0;
 }
